8d12cb9b046407bccc2c15bdafd1d3c9a96dc4a8
tupa1974 <tupa1974@gmail.com>
2022-09-20 08:57:48 -0300

    cambio de links en componentes audio y video

--- ejercicios/src/components/Audio/Audio.js
+++ ejercicios/src/components/Audio/Audio.js
@@ -5,7 +5,10 @@ export default class Audio extends Component {
 
     render() {
         return(
-               <audio className="audio"src="../../audios/Pajaros de Barro - Manolo Garcia.m4a" controls></audio>
+            <audio className="audio" controls>
+                <source src="https://www.w3schools.com/tags/horse.mp3" type="audio/ogg"/>
+            </audio>
+               
         );
     }
 }
--- ejercicios/src/components/Video/Video.js
+++ ejercicios/src/components/Video/Video.js
@@ -4,12 +4,9 @@ import "./Video.css";
 export default class Video extends Component {
   render() {
     return (
-      <video
-        width="320"
-        height="240"
-        controls
-        src="https://www.youtube.com/embed/videoseries?list=PL9LIrEiirmm9kpKuLAjXeu7QVXW8cxut6"
-      ></video>
+      <video width="320" height="240" controls>
+          <source src="https://www.w3schools.com/html/mov_bbb.mp4"type="video/mp4"/>
+      </video>
     );
   }
 }
